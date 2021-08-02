3fd7154ea231cd94849f0d9b312b6eb894738536
Crowdbotics <team@crowdbotics.com>
2021-08-02 23:02:22 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "withered_glitter_29353Identifier",
+  url: "https://withered_glitter_29353Identifier.botics.co",
+  api: "https://withered_glitter_29353Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
